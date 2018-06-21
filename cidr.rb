9146e4e3f259107672af03840a98997d8eaf6080
goreleaserbot <goreleaser@carlosbecker.com>
2018-06-21 01:30:28 +0100

    Brew formula update for cidr version v0.1.2

--- cidr.rb
+++ cidr.rb
@@ -1,9 +1,9 @@
 class Cidr < Formula
   desc "CIDR is a simple utility to generate the IPv4 addresses in a CIDR range. It could also be used to check the membership of an IP v4 address in a CIDR range"
   homepage "https://github.com/adedayo/cidr"
-  url "https://github.com/adedayo/cidr/releases/download/v0.1.1/cidr_0.1.1_Darwin_x86_64.tar.gz"
-  version "0.1.1"
-  sha256 "4bd0fd170ee17af389faac4f7190fe47d0651f1cb99dd15a3f3e95d9c1d2e075"
+  url "https://github.com/adedayo/cidr/releases/download/v0.1.2/cidr_0.1.2_Darwin_x86_64.tar.gz"
+  version "0.1.2"
+  sha256 "81d5312e8290223beb0511a8556b9592d8adabe1cb19c67224be08c8cb0d3348"
 
   def install
     bin.install "cidr"
