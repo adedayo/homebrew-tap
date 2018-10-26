331f0beb3d7d3fe8acf5e9834c9a58c6da515644
goreleaserbot <goreleaser@carlosbecker.com>
2018-10-26 20:44:13 +0100

    Brew formula update for sshscan version v0.1.2

--- sshscan.rb
+++ sshscan.rb
@@ -1,9 +1,9 @@
 class Sshscan < Formula
   desc "SSHScan is a simple utility for inspecting SSH key exchange settings on SSH servers"
   homepage "https://github.com/adedayo/sshscan"
-  url "https://github.com/adedayo/sshscan/releases/download/v0.1.1/sshscan_0.1.1_Darwin_x86_64.tar.gz"
-  version "0.1.1"
-  sha256 "114a06b2d416f464bdf62075e94a223b2598d6480e19e408c45fb9a531012399"
+  url "https://github.com/adedayo/sshscan/releases/download/v0.1.2/sshscan_0.1.2_Darwin_x86_64.tar.gz"
+  version "0.1.2"
+  sha256 "39e7c92da51c0ad3d6d0229ad31c30f65a9eec6a4a03ee6372d5e894bfd87a1f"
 
   def install
     bin.install "sshscan"
