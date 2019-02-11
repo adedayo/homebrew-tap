ddfec0f39874a46edd56cced725b30976329d8ba
goreleaserbot <goreleaser@carlosbecker.com>
2019-02-11 12:31:27 +0000

    Brew formula update for tlsaudit version v0.2.1

--- tlsaudit.rb
+++ tlsaudit.rb
@@ -1,9 +1,9 @@
 class Tlsaudit < Formula
   desc "tlsaudit is a utility for auditing TLS (including SSL and STARTTLS) security settings. You can use it to enumerate protocols, ciphers and curves supported by an open TCP port."
   homepage "https://github.com/adedayo/tlsaudit"
-  url "https://github.com/adedayo/tlsaudit/releases/download/v0.2.0/tlsaudit_0.2.0_Darwin_x86_64.tar.gz"
-  version "0.2.0"
-  sha256 "f98709dcc58cc7d224d35c0935c14311450f4bfdb2b524db61bfb06f7882676a"
+  url "https://github.com/adedayo/tlsaudit/releases/download/v0.2.1/tlsaudit_0.2.1_Darwin_x86_64.tar.gz"
+  version "0.2.1"
+  sha256 "88c14a1f551f18e15d760c9666487136dde6e61f608f3bf0a47671491dc3e6d0"
 
   def install
     bin.install "tlsaudit"
