731a9b500c9095b4b9db52441008dd9e2b6dd99f
goreleaserbot <goreleaser@carlosbecker.com>
2019-03-08 13:37:34 +0000

    Brew formula update for tcpscan version v0.5.6

--- tcpscan.rb
+++ tcpscan.rb
@@ -1,9 +1,9 @@
 class Tcpscan < Formula
   desc "TCPScan is a simple utility for discovering open (or closed) TCP ports on servers. It uses gopacket(https://github.com/google/gopacket) to craft SYN packets, listening asynchronously for (SYN-)ACK or RST responses without completing the full TCP handshake. TCPScan uses goroutines for asynchronous scans and it searches for the most likely listening ports first, using NMap's port frequency ordering. Anecdotal results show that TCPScan is really fast!"
   homepage "https://github.com/adedayo/tcpscan"
-  url "https://github.com/adedayo/tcpscan/releases/download/v0.5.1/tcpscan_0.5.1_Darwin_x86_64.tar.gz"
-  version "0.5.1"
-  sha256 "e63c66125d9dbc860df5c52204df76e46340d3a68fbb000ecc6101dc65223f8a"
+  url "https://github.com/adedayo/tcpscan/releases/download/v0.5.6/tcpscan_0.5.6_Darwin_x86_64.tar.gz"
+  version "0.5.6"
+  sha256 "d87753540d28a0e39013b8d9ec9085ec6763c43225123b50083c51ff57aab004"
 
   def install
     bin.install "tcpscan"
