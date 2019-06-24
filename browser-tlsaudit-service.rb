706d5e5161a2232b5aaba7449cf2c312711157f8
goreleaserbot <goreleaser@carlosbecker.com>
2019-06-24 04:01:39 +0100

    Brew formula update for browser-tlsaudit-service version v0.2.1

--- browser-tlsaudit-service.rb
+++ browser-tlsaudit-service.rb
@@ -2,14 +2,14 @@
 class BrowserTlsauditService < Formula
   desc "This is a simple services to collect TLS capabilities of a client browser"
   homepage "https://github.com/adedayo/browser-tls-audit"
-  version "0.2.0"
+  version "0.2.1"
 
   if OS.mac?
-    url "https://github.com/adedayo/browser-tls-audit/releases/download/v0.2.0/browser-tlsaudit-service_0.2.0_Darwin_x86_64.tar.gz"
-    sha256 "ca957c53f13b8cf277fd878f4e13676635f5cbcc0ad9106422cf01f5a54b7dc8"
+    url "https://github.com/adedayo/browser-tls-audit/releases/download/v0.2.1/browser-tlsaudit-service_0.2.1_Darwin_x86_64.tar.gz"
+    sha256 "8d797d2eb46c2f3186d8750bcfb8eb8beee68cf271a78abd3c54bb04c2f08cc5"
   elsif OS.linux?
-    url "https://github.com/adedayo/browser-tls-audit/releases/download/v0.2.0/browser-tlsaudit-service_0.2.0_Linux_x86_64.tar.gz"
-    sha256 "b97d8b83aee184d2c06b9ce04506a6c9cd84a2e98a9dccb8f03b480cc23a2a13"
+    url "https://github.com/adedayo/browser-tls-audit/releases/download/v0.2.1/browser-tlsaudit-service_0.2.1_Linux_x86_64.tar.gz"
+    sha256 "0c46128f8923ce6e6db603d6cfac52e2d68c301146cab3fbfcbceb59e11d3790"
   end
 
   def install
