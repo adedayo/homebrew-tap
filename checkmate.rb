aec5e1e9e3305a8bb5878d57fb214377720bd1fd
Dayo Adetoye <dayo.dev@gmail.com>
2021-09-22 22:04:23 +0100

    Brew formula update for checkmate version v0.4.5

--- checkmate.rb
+++ checkmate.rb
@@ -5,25 +5,25 @@
 class Checkmate < Formula
   desc "CheckMate is a code security analysis tool."
   homepage "https://github.com/adedayo/checkmate"
-  version "0.4.4"
+  version "0.4.5"
   license "BSD-3-Clause"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/adedayo/checkmate/releases/download/v0.4.4/checkmate_0.4.4_Darwin_x86_64.tar.gz"
-    sha256 "13df8adf09ffc5d0829bd5687d71cdf9082478de0885e025855ba162ec56c0e9"
+    url "https://github.com/adedayo/checkmate/releases/download/v0.4.5/checkmate_0.4.5_Darwin_x86_64.tar.gz"
+    sha256 "36e3532c1bc78fa97f67b17f76f5568ddd5d2b351d6e04d7969ddae7c251e36a"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/adedayo/checkmate/releases/download/v0.4.4/checkmate_0.4.4_linux_x86_64.tar.gz"
-    sha256 "2cbb3525c12a0fb6b3c98b4667cd010f9eb3e95ceb0adc94976f9a7c49a58ff0"
+    url "https://github.com/adedayo/checkmate/releases/download/v0.4.5/checkmate_0.4.5_linux_x86_64.tar.gz"
+    sha256 "6adc3efb8cc287a9b79b8a92538f230805abbd535d18212bec45e135c2b9791f"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/adedayo/checkmate/releases/download/v0.4.4/checkmate_0.4.4_linux_armv6.tar.gz"
-    sha256 "5a120b30f157fbce649ceb95b6d75f102139204bb6c5535f9a3a8ae137f16cec"
+    url "https://github.com/adedayo/checkmate/releases/download/v0.4.5/checkmate_0.4.5_linux_armv6.tar.gz"
+    sha256 "f8f5b7a27365db900f97835c43907c3bc189bd9d4c38985b49f3dc9e694d2fbd"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/adedayo/checkmate/releases/download/v0.4.4/checkmate_0.4.4_linux_arm64.tar.gz"
-    sha256 "f95edff4ed0a8343c2addd98057f2c5166b6566156cf11362681b40d1097c394"
+    url "https://github.com/adedayo/checkmate/releases/download/v0.4.5/checkmate_0.4.5_linux_arm64.tar.gz"
+    sha256 "0432e91d860f510efc122baf5aa61b6ca9b2936c74172f539a0584fd66abc043"
   end
 
   def install
