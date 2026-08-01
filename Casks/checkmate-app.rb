8f041965071f349f183bdb820e9a742f2fc887eb
Adedayo Adetoye <3177640+adedayo@users.noreply.github.com>
2026-08-01 15:24:21 +0100

    fix(cask): use strict sha256 checksum instead of no_check

--- Casks/checkmate-app.rb
+++ Casks/checkmate-app.rb
@@ -1,6 +1,6 @@
 cask "checkmate-app" do
   version "2.0.8"
-  sha256 :no_check
+  sha256 "9b5d7eeadb3559adbf56df8cd0de46ba57ca1f5e029e9e1687061dd7ec89ad1d"
 
   url "https://github.com/adedayo/checkmate-app/releases/download/v2.0.8/checkmate-app-macos-universal.dmg"
   name "CheckMate"
