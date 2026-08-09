9630ad936a2eda29afa17c2da55dd38ff1e3eb55
Dr. Adedayo Adetoye (aka Dayo) <3177640+adedayo@users.noreply.github.com>
2026-08-09 23:09:54 +0100

    checkmate-app v2.2.0

--- Casks/checkmate-app.rb
+++ Casks/checkmate-app.rb
@@ -1,8 +1,8 @@
 cask "checkmate-app" do
-  version "2.1.0"
-  sha256 "b6f2bcd8828c24793c70544dcab1ce3e637148c46e95473b7522ecb3466d92fe"
+  version "2.2.0"
+  sha256 "3cfbf5f888007f2d23830d3df94ce83c368d2727660375eb1511cf3d1a484949"
 
-  url "https://github.com/adedayo/checkmate-app/releases/download/v2.1.0/CheckMate-macos-universal.dmg"
+  url "https://github.com/adedayo/checkmate-app/releases/download/v2.2.0/CheckMate-macos-universal.dmg"
   name "CheckMate"
   desc "CheckMate Desktop Application"
   homepage "https://github.com/adedayo/checkmate-app"
